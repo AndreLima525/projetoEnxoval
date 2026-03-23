e452d65fb9038fa290fc92e8db10fec2dd77b617
Andre <andre525luis@gmail.com>
2026-03-23 13:39:56 -0300

    ok

--- view/index.php
+++ view/index.php
@@ -4,8 +4,32 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Página Inicial</title>
+	<link rel="stylesheet" type="text/css" href="../styles/styleIndex.css">
 </head>
 <body>
+	<div class="main">
+		<div class="card">
+			<div class="title">
+				<h1>Laura e André </h1>
+				<p class="sub">Chá de Casa Nova • Lista de Presentes</p>
+			</div>
+			<div class="img">
+				<img src="../images/fotoCasal.jpeg">
+			</div>
+			<div class="form">
+				<form method="POST">
+					<label>Nome</label>
+					<input type="text" name="nome">
 
+					<label>Número de Telefone</label>
+					<input type="text" name="telefone">
+
+					<button name="avancar">
+						Avançar
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
