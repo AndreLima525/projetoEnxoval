c763514d88c023468f50909e2969b00faf32a1a9
Andre <andre525luis@gmail.com>
2026-07-04 23:56:41 -0300

    ok

--- controller/confirmaPresenteController.php
+++ controller/confirmaPresenteController.php
@@ -4,7 +4,9 @@ session_start();
 
 include_once('../model/confirmaPresenteModel.php');
 
-
+if (!isset($_SESSION['idPresente'])) {
+	header("location:../view/main.php");
+}
 
 $email = $_GET['email'];
 $idPresente = $_GET['idPresente'];
